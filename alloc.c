91795cac0f345094d357df866029e8b1fdbfa0c9
kojjok97 <kojjok971@naver.com>
2023-01-22 08:05:12 +0900

    [DynamicAllocator][BoB과제] 요구사항 및 TODO 작성

--- alloc.c
+++ alloc.c
@@ -1,6 +1,30 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+/***할것 
+ * 1. 구조체 만들기 (header와 footer 읽을 수 있는?)
+ * 2. 구조체를 통해서 앞 뒤 메모리 연결 
+ * 3. alloc 구현 
+ *  - 가장 적합한 메모리 찾기 Best fit(메모리 효율 > 처리 속도) 
+ *  - 메모리 사이즈 입력 
+ *  - free 연결 리스트 구조 변경 
+ * 4. free 구현 
+ *  - free 메모리 정보 입력
+ *  - 메모리 정리 앞 뒤 free 영역 합치기  
+ *  - free 연결 리스트 추가 
+ * 5. realloc 
+ *  - 음... 고민중 
+ * 6. 상세 구현 alloc 
+ *  - sbrk에 대해 알필요가 있음 현재 location? or size 더한 location? 
+ *  - sbrk 0 할당? 
+ *  - 받은 포인터를 구조체를 통해 정보 입력 앞뒤 size+ 헤더 푸터 
+ *  - free 연결리스트 구조체 변경 함수 구현 
+ *  - return p;
+ *  TODO 
+ *  - 구현 해야하는 부분 크게 나누기
+ *  - 구현 해야하는 부분 작게 나누기 세분화 
+ *  - 설계 작성  
+***/
 
 extern void debug(const char *fmt, ...);
 extern void *sbrk(intptr_t increment);
