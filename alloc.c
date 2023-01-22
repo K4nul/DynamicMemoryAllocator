157b353174018ebcf4a18eed960a6f3dc8d0c037
kojjok97 <kojjok971@naver.com>
2023-01-22 14:50:27 +0900

    [DynamicAllocator][BoB과제] 요구사항 작성중

--- alloc.c
+++ alloc.c
@@ -1,12 +1,15 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
-/***할것 
- * 1. 구조체 만들기 (header와 footer 읽을 수 있는?)
+/*** 요구사항 
+ * 1. 구조체 만들기 (header와 footer 읽을 수 있게)
+ *  - 구조체 리스트 header 구조체 (footer와 정보는 같음)
+ *  - 몇개 필요한지 조사
  * 2. 구조체를 통해서 앞 뒤 메모리 연결 
  * 3. alloc 구현 
- *  - 가장 적합한 메모리 찾기 Best fit(메모리 효율 > 처리 속도) 
+ *  - 가장 적합한 메모리 찾기 Best fit segregated list 구현시 first fit(메모리 효율 > 처리 속도) 
  *  - 메모리 사이즈 입력 
+ *  - split 구현?
  *  - free 연결 리스트 구조 변경 
  * 4. free 구현 
  *  - free 메모리 정보 입력
@@ -20,10 +23,19 @@
  *  - 받은 포인터를 구조체를 통해 정보 입력 앞뒤 size+ 헤더 푸터 
  *  - free 연결리스트 구조체 변경 함수 구현 
  *  - return p;
+ * 7. 할당 실패 
+ *  - 할당 실패에 대한 예외 처리
+ * 8. Segregated List 구현 
+ *  - 각 크기별 free list 구현 
  *  TODO 
  *  - 구현 해야하는 부분 크게 나누기
  *  - 구현 해야하는 부분 작게 나누기 세분화 
- *  - 설계 작성  
+ *  - 설계 작성
+ * allocator 동작 흐름 
+ *  - 메모리 사이즈를 받는다 
+ *  - 메모리가 없다면 sbrk를 통해 힙영역 공간 추가 (최초 init 이후 공간 할당시 확인 이후 공간 확장 혹은 free list에서 확인)  
+ *  - segregated List에서 크기를 비교해 적당한 크기의 공간을 할당 받는다 
+ *  - 할당시 헤더에 사이즈 정보와 할당 여부를 넣는다 (explicit free 시에는 안넣어도 될듯)
 ***/
 
 extern void debug(const char *fmt, ...);
